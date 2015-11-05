b8dcd4a006414f13c3b8804fb336d32043961bc7
benjamincampbell <twitch043@gmail.com>
2015-11-05 00:31:26 -0500

    Fixed error where doing the choose command with no arguments caused a crash

--- plugins/choose.py
+++ plugins/choose.py
@@ -4,8 +4,9 @@ def choose(nick, channel, message, handler):
     choices = message.split(sep = ",")
     #this line removes empty strings from consecutive commas
     choices = [i for i in choices if i != '']
-    choice = random.sample(choices, 1)
-    #this line makes the choice string look better, no leading spaces or brackets
-    winner = "".join(choice).strip()
-
-    handler.privmsg(channel, "{}: {}".format(nick, winner))
+    try:
+        choice = random.sample(choices, 1)
+        winner = "".join(choice).strip()
+        handler.privmsg(channel, "{}: {}".format(nick, winner))
+    except:
+        handler.privmsg(channel, "{}: Please enter choices".format(nick))
