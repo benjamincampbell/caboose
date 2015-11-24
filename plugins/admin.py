231bfa87bf52460bffcc94fff51ef22474a8fc25
benjamincampbell <twitch043@gmail.com>
2015-11-24 13:13:17 -0500

    Cleaned up admin function, made into admin and unadmin

--- plugins/admin.py
+++ plugins/admin.py
@@ -1,33 +1,26 @@
 @command("admin", man = "Adds or removes people from admin list. Admin only. Usage: -admin add/remove name")
 def admin(nick, channel, message, handler):
-    try:
-        cmd, name = message.split()
+    if nick in handler.SETTINGS.admins:
+        toadmin = message.split()
+        with open("admins.txt", 'a') as f:
+            for nick in toadmin:
+                f.write('{}\n'.format(nick))
+        handler.SETTINGS.update()
+    else:
+        handler.privmsg(channel, '{}: You don\'t have permission to do that'.format(nick))
 
-        if nick in handler.SETTINGS.admins:
-            if cmd == "add":
-                with open("admins.txt", 'a') as r:
-                    r.write("name\n".format(name))
-                handler.privmsg(channel, '{}: {}'.format(nick, name + " added to admins"))
-            elif cmd == "remove":
-                if name != "twitch":
-                    with open("admins.txt", 'w') as r:
-                        for adminline in adminslist:
-                            if adminline != name:
-                                r.write(adminline + "\n")
-                    handler.privmsg(channel, '{}: {}'.format(nick, name + " removed from admins"))
-                else:
-                    handler.privmsg(channel, '{}: {}'.format(nick, "Can't let you do that, Star Fox."))
-            else:
-                pass
-        else:
-            pass
-    
-        with open("admins.txt", 'r') as r: 
-            adminslist = r.read().splitlines()
-        print("Admins:")
-        print(adminslist)
-    except ValueError:
-        handler.privmsg(channel, '{}: One at a time, please, I\'m not a barbarian'.format(nick))
+@command("unadmin", man = "Removes people from admin list. Admin only. Usage: -unadmin nick1")
+def unadmin(nick, channel, message, handler):
+    if nick in handler.SETTINGS.admins:
+        with open("admins.txt", 'r') as f:
+            adminslist = f.read().splitlines()
+        with open("admins.txt", 'w') as f:
+            for nick in adminslist:
+                if nick not in message.split():
+                    f.write("{}\n".format(nick))
+        handler.SETTINGS.update()
+    else:
+        handler.privmsg(channel, '{}: You don\'t have permission to do that'.format(nick))
 
 @command("join", man = "Makes Caboose join a channel. Admin only. Usage: -join #channel  (must type #)")
 def join(nick, channel, message, handler):
@@ -54,9 +47,7 @@ def quit(nick, channel, message, handler):
 @command("ignore", man = "Makes Caboose ignore one or more nicks. Admin only. Usage: -ignore nick1 nick2")
 def ignore(nick, channel, message, handler):
     if nick in handler.SETTINGS.admins:
-        toignore = []
-        for nick in message.split():
-            toignore.append(nick)
+        toignore = message.split()
         with open("ignore.txt", 'a') as f:
             for nick in toignore:
                 f.write('{}\n'.format(nick))
