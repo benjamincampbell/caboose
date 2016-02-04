bb0c9575b391723046f0368d208691715f7ceb2c
Benjamin Campbell <twitch@yakko.cs.wmich.edu>
2016-02-04 12:08:44 -0500

    Ops working

--- plugins/op.py
+++ plugins/op.py
@@ -1,10 +1,15 @@
 @command("op", man = "Gives levels of ops to a user. -h to give half ops Usage: &ops [-h] user")
 def op(nick, channel, message, handler):
 	opstring = "+"
+	count = 0
+	for user in message.split():
+            count += 1
 
-	opstring += "o" for user in message.split()
+	for i in range(count):
+            opstring += "o"
 	opstring += " "
-	opstring += "%s " % user for user in message.split()
+	for user in message.split():
+            opstring += "%s " % user
 
 	if nick in handler.SETTINGS.admins:
 		handler.sendraw("MODE %s %s\r\n" % (channel, opstring))
