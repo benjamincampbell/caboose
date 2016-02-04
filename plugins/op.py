9682e66824daeceb8cf1c4b155b729a4f4dfa4e3
Benjamin M. Campbell <benjamin.m.campbell@wmich.edu>
2016-02-04 11:50:38 -0500

    work on op function

--- plugins/op.py
+++ plugins/op.py
@@ -1,6 +1,12 @@
 @command("op", man = "Gives levels of ops to a user. -h to give half ops Usage: &ops [-h] user")
 def op(nick, channel, message, handler):
+	opstring = "+"
+
+	opstring += "o" for user in message.split()
+	opstring += " "
+	opstring += "%s " % user for user in message.split()
+
 	if nick in handler.SETTINGS.admins:
-		handler.sendraw("MODE %s +o %s\r\n" % (channel, nick))
+		handler.sendraw("MODE %s %s\r\n" % (channel, opstring))
 	else:
 		handler.privmsg(channel, '{}: You don\'t have permission to do that'.format(nick))
