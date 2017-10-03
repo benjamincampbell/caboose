4ece4bb265d857696f1add7532a3edd8f3e4d282
Benjamin Mark Campbell <bgf0186@WADE.WMICH.EDU>
2017-10-03 17:30:23 -0400

    Working on line object

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,4 +1,4 @@
-
+import re
 
 class Line(raw):
     """
@@ -9,17 +9,25 @@ class Line(raw):
     Sample: :Twitch!~twitch@hostname.com PRIVMSG #channel :Test response
     """
     
-    def __init__(self):
-        self.nick = None
-        self.user = None
-        self.host = None
+    def __init__(self, raw):
         self.type = None
         self.channel = None
+        self.command = None
+        self.text = None
+        self.user = UserInfo(raw.split(' ', 1)[0][1:])
+        
+        parse_line(raw.split(' ', 1)[1])
+        
+    
+    def parse_line(raw):
+        
         
 class UserInfo:
-    def __init__(self):
+    def __init__(self, raw_user):
         self.nick = None
         self.user = None
         self.host = None
         
-        
+    def parse_user(raw_user):
+        match = re.search('^([^!]+)!([^@]+)@(.+)$', raw_user)
+        if match:
