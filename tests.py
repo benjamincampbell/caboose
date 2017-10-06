8759a8c986b87f7684e7f831854e5ebf3efd8080
Benjamin Campbell <benjamin.m.campbell@wmich.edu>
2017-10-05 23:47:26 -0400

    correct yaml read-in test now works

--- tests.py
+++ tests.py
@@ -30,22 +30,22 @@ class TestIrcLineParsing(unittest.TestCase):
         self.assertEqual(l.text, 'irc.example.com')
         
 class TestReadingConfig(unittest.TestCase):
-    def test_reading_yaml(self):
+    def test_reading_correct_yaml(self):
         f = """settings:
     nick: caboose
     leader: "!"
 connections:
-    valinor:
-        host: valinor.worf.co
+    test1:
+        host: irc.testserver1.org
         port: 42697
-        pwd: connectpw1
+        pwd: testpass
         ssl: true
         admins:
           - twitch
         channels:
           - "#caboose"
-    snoonet:
-        host: irc.snoonet.org
+    test2:
+        host: irc.testserver2.org
         port: 6667
         pwd: ~
         ssl: false
@@ -57,12 +57,27 @@ connections:
         servers = {}
         
         pp = pprint.PrettyPrinter(indent=4)
+        
         pp.pprint(cfg)
         
         for name, settings in cfg['connections'].items():
-            server = Server(name, **settings)
+            servers[name] = Server(name, **settings)
+            pp.pprint(vars(servers[name]))
+            
+        self.assertEqual(servers['test1'].NAME, 'test1')
+        self.assertEqual(servers['test1'].HOST, 'irc.testserver1.org')
+        self.assertEqual(servers['test1'].PASS, 'testpass')
+        self.assertEqual(servers['test1'].SSL, True)
+        self.assertEqual(servers['test1'].ADMINS, ['twitch'])
+        self.assertEqual(servers['test1'].CHANNELS, ['#caboose'])
         
-        pp.pprint(vars(server))
+        self.assertEqual(servers['test2'].NAME, 'test2')
+        self.assertEqual(servers['test2'].HOST, 'irc.testserver2.org')
+        self.assertEqual(servers['test2'].PASS, None)
+        self.assertEqual(servers['test2'].SSL, False)
+        self.assertEqual(servers['test2'].ADMINS, ['notlikethesoup'])
+        self.assertEqual(servers['test2'].CHANNELS, ['#caboose'])
+        
         
 if __name__ == '__main__':
     unittest.main()
