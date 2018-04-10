5e914b7009de1ace7b621182ae977bfd2be2d454
Benjamin Campbell <twitch@urt-server.localdomain>
2018-04-10 19:37:45 -0400

    renamed help.py to commands.py, the command name changed as well

---
+++ plugins/commands.py
@@ -0,0 +1,5 @@
+@command("commands", man = "Displays list of commands. Usage: {leader}{command}")
+def commands(bot, line):
+    line.conn.privmsg(line.args[0], "Available commands: {}".format(", ".join(key for key 
+        in bot.COMMANDS if (key != "__builtins__" and bot.COMMANDS[key].visible))))
+    #Add functionality to display ** after command if it's disabled (footnote after explaining so)
--- plugins/help.py
+++
@@ -1,5 +0,0 @@
-@command("help", man = "Displays list of commands. Usage: {leader}{command}")
-def help(bot, line):
-    line.conn.privmsg(line.args[0], "Available commands: {}".format(", ".join(key for key 
-        in bot.COMMANDS if (key != "__builtins__" and bot.COMMANDS[key].visible))))
-    #Add functionality to display ** after command if it's disabled (footnote after explaining so)
