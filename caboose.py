087a9cfbe3d320acd30d8ff299777fff5ecc8bf9
Benjamin Campbell <benjamin.m.campbell@wmich.edu>
2018-05-08 00:23:00 -0400

    Similar function working, maybe flesh out errors

--- caboose.py
+++ caboose.py
@@ -2,8 +2,18 @@ import bot.command
 import bot.core
 import logging
 import os
+import sys
 
-logging.basicConfig(filename=os.path.abspath('caboose.log'),level=logging.DEBUG)
+if sys.argv[1] == "DEBUG":
+    logging.basicConfig(filename=os.path.abspath('caboose.log'),level=logging.DEBUG)
+elif sys.argv[1] == "INFO":
+    logging.basicConfig(filename=os.path.abspath('caboose.log'),level=logging.INFO)
+elif sys.argv[1] == "WARNING":
+    logging.basicConfig(filename=os.path.abspath('caboose.log'),level=logging.WARNING)
+elif sys.argv[1] == "ERROR":
+    logging.basicConfig(filename=os.path.abspath('caboose.log'),level=logging.ERROR)
+else:
+    logging.basicConfig(filename=os.path.abspath('caboose.log'),level=logging.INFO)
 
 if __name__ == "__main__":
 
--- plugins/lastfm.py
+++ plugins/lastfm.py
@@ -10,15 +10,15 @@ def lastfm(bot, line):
     # testing
     import pprint
 
-    RECENT_TRACK_URL = "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user={0}&api_key={1}&format=json"
-    TRACK_INFO_URL = "http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key={0}&artist={1}&track={2}&username={3}&format=json"
+    RECENT_TRACK_URL = "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user={user}&api_key={api_key}&format=json"
+    TRACK_INFO_URL = "http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key={api_key}&artist={artist}&track={track}&username={user}&format=json"
     API_KEY = bot.SECRETS["api_keys"]["lastfm"]
 
     parts = line.text.split(' ')
 
     username = parts[0]
 
-    recent_response = requests.get(RECENT_TRACK_URL.format(username, API_KEY))
+    recent_response = requests.get(RECENT_TRACK_URL.format(user=username, api_key=API_KEY))
     recent_json = json.loads(recent_response.text)
 
     try:
@@ -51,8 +51,8 @@ def lastfm(bot, line):
                 lastfm_track_time = lastfm_track_info["date"]["#text"]
                 now_playing = False
 
-            info_response = requests.get(TRACK_INFO_URL.format(API_KEY, lastfm_track_artist,
-                lastfm_track_song, last_fm_track_user))
+            info_response = requests.get(TRACK_INFO_URL.format(api_key=API_KEY, artist=lastfm_track_artist,
+                track=lastfm_track_song, user=last_fm_track_user))
             info_json = json.loads(info_response.text)
 
             last_fm_track_tags = []
@@ -89,3 +89,45 @@ def lastfm(bot, line):
         msg = "User {0} does not exist.".format(color(username, 'green'))
 
     line.conn.privmsg(line.args[0], msg)
+
+@command("similar", man = "Obtain artists similar to the given artist. Usage: {leader}{command} <artist>")
+def similar(bot, line):
+    import json
+    import requests
+    from datetime import datetime
+    from bot.colors import color
+
+    SIMILAR_URL = "http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist={artist}&api_key={api_key}&format=json"
+    API_KEY = bot.SECRETS["api_keys"]["lastfm"]
+
+    artist = line.text
+
+    if artist.strip() == "":
+        line.conn.privmsg(line.args[0], "Please enter an artist.")
+    else:
+        similar_response = requests.get(SIMILAR_URL.format(artist=artist, api_key=API_KEY))
+        similar_json = json.loads(similar_response.text)
+
+        msg = ""
+        i = 0
+
+        if "similarartists" in similar_json:
+            if "artist" in similar_json["similarartists"]:
+                if similar_json["similarartists"]["@attr"]["artist"] == "[unknown]":
+                    logging.warning("API Error: Unknown artist, but not Error code 6")
+                    msg = "Artist {0} not found.".format(color(artist, 'green'))
+                else:
+                    msg = "Artists similar to {0}:".format(color(similar_json["similarartists"]["@attr"]["artist"], 'green'))
+                    for a in similar_json["similarartists"]["artist"]:
+                        if i < 8:
+                            msg += " {0},".format(color(a["name"], 'lightblue'))
+                            i += 1
+            else:
+                logging.warning("API Error: no ['artist'] key for {artist}".format(artist=artist))
+        else:
+            # Some sort of error in JSON
+            if "error" in similar_json:
+                if similar_json["error"] == 6:
+                    msg = "Artist {0} not found.".format(color(artist, 'green'))
+            logging.warning("API Error: no ['similarartists'] key for {artist}".format(artist=artist))
+        line.conn.privmsg(line.args[0], msg[:-1] if msg[-1]==',' else msg)
