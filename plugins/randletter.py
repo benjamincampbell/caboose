84af4d8679339a8eca83158d1910a20245c55568
Matt Provenzale <matthew.provenzale@gmail.com>
2024-01-04 16:14:12 -0500

    adding up to 5 random letters printed

--- plugins/randletter.py
+++ plugins/randletter.py
@@ -1,9 +1,23 @@
 from bot.command import command
 
-@command("randletter", man = "Chooses a random letter. Usage: {leader}{command} <message>")
+@command("randletter", man = "Chooses a random letter. Optionally supply number to return multiple letters. Usage: {leader}{command} <number>")
 def randletter(bot, line):
     import string
     import random
-    text = random.choice(string.ascii_uppercase)
-    
-    line.conn.privmsg(line.args[0], text)
+
+    linesplit = line.text.split()
+    if len(linesplit) > 1:
+        # too many arguments
+        line.conn.privmsg(line.user.nick, "Too many arguments supplied")
+        return None
+    elif len(linesplit) == 1:
+        if linesplit[0].isdigit() and int(linesplit[0]) < 6:
+            num = int(linesplit[0])
+        else:
+            line.conn.privmsg(line.user.nick, "Please enter a number 5 or fewer.")
+
+    # else no args
+
+    text = random.sample(string.ascii_uppercase, num)
+
+line.conn.privmsg(line.args[0], text)
